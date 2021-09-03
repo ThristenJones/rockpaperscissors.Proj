8ec5658cd7f687c04728622fd4a28bcf063282e1
ThristenJones <Thristenjones@yahoo.com>
2021-09-02 20:49:09 -0500

    Added new files to the project

---
+++ game.js
@@ -0,0 +1,5 @@
+class Game {
+    constructor(array)
+}this.array = [rock.paper,scissors,lizard,spock]
+
+module.exports.game.array
---
+++ player.js
@@ -0,0 +1,10 @@
+let gameArray = require('./game');
+
+class Player {
+ constructor(name){
+this.name = name
+}
+
+displayFunction()
+array.length
+}
