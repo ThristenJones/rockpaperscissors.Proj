828b3a9d9ddbfb4a5e67f3e1c05ea5b1a22e4eee
ThristenJones <Thristenjones@yahoo.com>
2021-09-08 17:32:30 -0500

    Added a new index.js file

--- game.js
+++ game.js
@@ -1,5 +1,12 @@
+
+
 class Game {
     constructor(array)
 }this.array = [rock.paper,scissors,lizard,spock]
 
+let rules = new Game()
+
+
 module.exports.game.array
+
+
--- player.js
+++ player.js
@@ -6,7 +6,8 @@ this.name = name
 }
 
 displayFunction(){
-for (let i = 0; i < gameArray.array.length; i++)
+for (let i = 0; i < gameArray.array.length; i++);
+}
 }
 
 module.exports.player
